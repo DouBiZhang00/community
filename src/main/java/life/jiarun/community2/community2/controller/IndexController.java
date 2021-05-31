778d65d8dbae69037c2a25cd70ee1ffbd2d114f6
jiarun <zjr546995978@gamil.com>
2021-06-01 01:03:32 +0800

    add index and bootstrap

---
+++ src/main/java/life/jiarun/community2/community2/controller/IndexController.java
@@ -0,0 +1,13 @@
+package life.jiarun.community2.community2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String hello(){return "index";}
+}
