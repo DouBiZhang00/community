b7d06ac1ba6effa1f97de36306f4bd5492907e7d
jiarun <zjr546995978@gamil.com>
2021-06-01 19:25:41 +0800

    extract github config from to properties and OAuth from github

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## 菜鸟社区
 
 ##资料
-[Spring 文档](https://spring.io/guides)
-[Spring Web](https://spring.io/guides/gs/serving-web-conteng/)
-[es](https://elasticsearch.cn/explore)
-[Bootstrap](https://v3.bootcss.com/getting-started)
+[Spring 文档](https://spring.io/guides) <br>
+[Spring Web](https://spring.io/guides/gs/serving-web-conteng/) <br>
+[elasticsearch](https://elasticsearch.cn/explore) <br>
+[Bootstrap](https://v3.bootcss.com/getting-started) <br>
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 
 ##工具
-[Git](https://git-scm.com/download)
-[Visual Paradigm](https://www.visual-paradigm.com)
+[Git](https://git-scm.com/download) <br>
+[Visual Paradigm](https://www.visual-paradigm.com) <br>
---
+++ src/main/java/life/jiarun/community2/community2/controller/AuthorizeController.java
@@ -0,0 +1,42 @@
+package life.jiarun.community2.community2.controller;
+
+import life.jiarun.community2.community2.dto.AccessTokenDTO;
+import life.jiarun.community2.community2.dto.GithubUser;
+import life.jiarun.community2.community2.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        return "index";
+    }
+}
